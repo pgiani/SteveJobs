c47aefe5625972f8ff06424ea9db757c4b0ec12a
pablo Giani <pgiani@gmail.com>
2019-06-06 11:02:23 -0600

    add stack trace

--- package/server/imports/actions/execute/process.js
+++ package/server/imports/actions/execute/process.js
@@ -1,36 +1,37 @@
-import { Utilities } from "../../utilities"
-import { toolbelt } from "./toolbelt.js"
-import { reschedule } from "../reschedule/"
+import { Utilities } from '../../utilities';
+import { toolbelt } from './toolbelt.js';
+import { reschedule } from '../reschedule/';
 
-var process = function (doc, callback) {
-	// Goals: 
-	// 1- Execute the job
-	// 2- Update the document in database
-	// 3- Capture the result (if any)
+var process = function(doc, callback) {
+  // Goals:
+  // 1- Execute the job
+  // 2- Update the document in database
+  // 3- Capture the result (if any)
 
-	var Toolbelt = new toolbelt(doc);
+  var Toolbelt = new toolbelt(doc);
 
-	try {
-		var jobResult = Utilities.registry.data[doc.name].apply(Toolbelt, doc.arguments);
-		var resolution = Toolbelt.checkForResolution();
+  try {
+    var jobResult = Utilities.registry.data[doc.name].apply(
+      Toolbelt,
+      doc.arguments,
+    );
+    var resolution = Toolbelt.checkForResolution();
 
-		if (typeof callback === "function") {
-			return callback(undefined, jobResult);
-		} else {
-			return jobResult;
-		}
-	}
+    if (typeof callback === 'function') {
+      return callback(undefined, jobResult);
+    } else {
+      return jobResult;
+    }
+  } catch (e) {
+    // var failure = Toolbelt.failure();
+    var failure = Toolbelt.failure(e.stack);
+    Utilities.logger('Job failed to run due to code error: ' + doc.name);
+    console.log(e);
 
-	catch (e) {		
-		var failure = Toolbelt.failure();
-		
-		Utilities.logger("Job failed to run due to code error: " + doc.name)
-		console.log(e);
+    if (typeof callback === 'function') {
+      return callback(true, undefined);
+    }
+  }
+};
 
-		if (typeof callback === "function") {
-			return callback(true, undefined);
-		}
-	}
-}
-
-export { process }
+export { process };
