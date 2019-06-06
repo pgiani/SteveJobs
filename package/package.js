76c264b87246a803c15b85b788f2a250d331d29a
pablo Giani <pgiani@gmail.com>
2019-06-06 10:59:24 -0600

    test

--- package/package.js
+++ package/package.js
@@ -1,14 +1,15 @@
 Package.describe({
-	name:    "msavin:sjobs",
-	summary: "The simple jobs queue that just works [synced, schedule, tasks, background, later, worker, cron]",
-	version: "4.0.0",
-	documentation: "README.md",
-	git:     "https://github.com/msavin/SteveJobs.git",
+  name: 'msavin:sjobs',
+  summary:
+    'The simple jobs queue that just works [synced, schedule, tasks, background, later, worker, cron]',
+  version: '3.1.0',
+  documentation: 'README.md',
+  git: 'https://github.com/msavin/SteveJobs.git',
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.3');
-	api.use(["mongo", "random", "ecmascript", "check"], "server");
-	api.mainModule("server/api.js", "server");
-	api.export(["Jobs", "JobsInternal"]);
+  api.versionsFrom('1.3');
+  api.use(['mongo', 'random', 'ecmascript', 'check'], 'server');
+  api.mainModule('server/api.js', 'server');
+  api.export(['Jobs', 'JobsInternal']);
 });
